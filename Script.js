d1ce66ee11ba48fa3c1fa4e29c797c1b96f94a19
Ikrion <zhanghaien100@gmail.com>
2023-08-07 12:40:04 +0800

    upload latest

--- Script.js
+++ Script.js
@@ -1,36 +1,46 @@
 //target all elements to save to constants
-const page1btn=document.querySelector("#page1btn");
-const page2btn=document.querySelector("#page2btn");
-const page3btn=document.querySelector("#page3btn");
-var allpages=document.querySelectorAll(".page");
+const section1btn=document.querySelector("#section1btn");
+const section2btn=document.querySelector("#section2btn");
+const section3btn=document.querySelector("#section3btn");
+const allsection=document.querySelectorAll(".section");
+const allcomponent=document.querySelectorAll(".components");
+
 //select all subtopic pages
-console.log(allpages);
+console.log(allsection);
 hideall();
 
 function hideall(){ //function to hide all pages
-	for(let onepage of allpages){ //go through all subtopic pages
-	onepage.style.display="none"; //hide it
+	for(let onesection of allsection){ //go through all subtopic pages
+		onesection.style.display="none"; //hide it
 	}
 }
 
 function show(pgno){ //function to show selected page no
 	hideall();
 	//select the page based on the parameter passed in
-	let onepage=document.querySelector("#page"+pgno);
-	//show the page
-	onepage.style.display="block";
+	let onesection=document.querySelector("#section"+pgno);
+	if (pgno =="2")
+	{
+		//show the page with flex display
+		onesection.style.display="flex";
+	}
+	else
+	{
+		//show the page
+		onesection.style.display="block";
+	}
 }
 
 /*Listen for clicks on the buttons, assign anonymous
 eventhandler functions to call show function*/
-page1btn.addEventListener("click", function () {
-show(1);
+section1btn.addEventListener("click", function () {
+	show(1);
 });
-page2btn.addEventListener("click", function () {
-show(2);
+section2btn.addEventListener("click", function () {
+	show(2);
 });
-page3btn.addEventListener("click", function () {
-show(3);
+section3btn.addEventListener("click", function () {
+	show(3);
 });
 
 /*for hamMenu */
@@ -38,8 +48,72 @@ const hamBtn=document.querySelector("#icon");
 hamBtn.addEventListener("click",toggleMenus);
 const menuItemsList=document.querySelector("nav ul");
 function toggleMenus(){ /*open and close menu*/
-	menuItemsList.classList.toggle("menuShow);
-// if(menuItemsList.style.display=="block")
-// menuItemsList.style.display="none";
-// else menuItemsList.style.display="block";
-}//can optimize using toggle class with css transitions
+	menuItemsList.classList.toggle("menuShow");
+}
+
+/* For image description */
+
+
+
+
+function showDes(str){
+	for(let onecomponent of allcomponent){ //go through all the components
+		onecomponent.style.display="none"; //hide it
+	}
+	switch(str){
+		case 'boots':
+			document.querySelector("#softBoots").style.display="block";
+			break;
+		case 'cuffs':
+			document.querySelector("#cuff").style.display="block";
+			break;
+		case 'fasteners':
+			document.querySelector("#fasteners").style.display="block";
+			break;
+		case 'shell':
+			document.querySelector("#shell").style.display="block";
+			break;
+		case 'brake':
+			document.querySelector("#brake").style.display="block";
+			break;
+		case 'wheels':
+			document.querySelector("#wheel").style.display="block";
+			break;
+		case 'wheelFrame':
+			document.querySelector("#frame").style.display="block";
+			break;
+		default:
+			break;
+	}
+}
+
+
+/* For Slideshow */
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
